a345da6760b8dac05bc329ffa271c999bfa4c5f1
choisumin <tnals634@naver.com>
2023-08-11 12:52:37 +0900

    feat: dto 사용해보기
    게시글crud 작성중 post, put, delete에 필요한 값을 받아내기 위해 dto를
    사용.
    입력값 유효성 검사를 하기위해 class-validator 설치, TypeScript 진영에서
    직렬화와 역직렬화를 위해 class-transformer설치,좀더 쉽게 사용하기 위해
    @nestjs/mapped-types 설치

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board') //routing path is /board -> e.g. http://localhost:3000/board
-export class BoardController {}
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
+  //게시글 목록을 가져오는 api
+  @Get('/articles')
+  getArticles() {
+    return this.boardService.getArticles();
+  }
+
+  //게시글 상세 보기
+  @Get('/articles/:id')
+  getArticleById() {
+    return this.boardService.getArticleById(id);
+  }
+
+  //게시글 작성
+  @Post('/articles')
+  createArticle() {
+    return this.boardService.createArticle();
+  }
+
+  //게시글 수정
+  @Put('/articles/:id')
+  updateArticle() {
+    return this.boardService.updateArticle(id);
+  }
+
+  //게시글 삭제
+  @Delete('/articles/:id')
+  deleteArticle() {
+    return this.boardService.deleteArticle(id);
+  }
+}
